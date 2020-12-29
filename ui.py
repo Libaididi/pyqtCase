be00ffadf7941b0c220fa12bb2d9aac6ef851f2f
libaididi <357869271@qqcom>
2020-12-29 19:25:32 +0800

    改变了文件

--- ui.py
+++ ui.py
@@ -31,8 +31,6 @@ if __name__ == '__main__':
     w.setWindowTitle('Simple')
     # 显示在屏幕上
     w.show()
-
-    # 改变
     # 进入程序主循环、并通过exit()确保主循环安全结束
     # 系统exit()方法确保应用程序干净的退出
     # 的exec_()方法有下划线。因为执行是一个Python关键词。因此，exec_()代替
